2d29f98de6752d5a8ce7fbd9ec0b5e1784e53415
Ankush Kalsi <ankushk456@gmail.com>
2019-03-08 15:18:05 +0530

    Update PF-Assgn20.py

--- infytq/PF/PF-Assgn20.py
+++ infytq/PF/PF-Assgn20.py
@@ -4,12 +4,15 @@ def calculate_loan(account_number,salary,account_balance,loan_type,loan_amount_e
     ela = 0 #eligible_loan_amount
     bee= 0 #bank_emi_expected
     ac = str(account_number) #account number to string
+    flag = 0
     if len(ac)!=4 or ac[0]!='1':
         print("Invalid account number")
+        return 0
     elif account_balance <100000:
         print("Insufficient account balance")
+        return 0
     else:
-        flag = 0
+        
         if loan_type =="Car":
             if salary <=25000:
                 flag=1
@@ -20,18 +23,20 @@ def calculate_loan(account_number,salary,account_balance,loan_type,loan_amount_e
             if salary <=50000:
                 flag = 1
             else:
-                ela = 600000
+                ela = 6000000
                 bee = 60
         elif loan_type == "Business":
             if salary <=75000:
                 flag = 1
             else:
-                ela = 750000
+                ela = 7500000
                 bee = 84
         else:
             print("Invalid loan type or salary")
+            return 0
     if flag ==1:
         print("Invalid loan type or salary")
+        return 0
     else:
         if ela<loan_amount_expected or bee<customer_emi_expected:
             print("The customer is not eligible for the loan")
@@ -42,4 +47,4 @@ def calculate_loan(account_number,salary,account_balance,loan_type,loan_amount_e
             print("Requested loan amount:", loan_amount_expected)
             print("Requested EMI's:",customer_emi_expected)
 
-calculate_loan(1001,40000,250000,"Car",300000,30)
+calculate_loan(2005,300000,255000,"Car",30000,30)
