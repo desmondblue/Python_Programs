b701b5ddc0c72b511e10eec002769acd30479f64
Ankush Kalsi <ankushk456@gmail.com>
2019-03-23 18:56:59 +0530

    Create PF-Assgn40.py

---
+++ infytq/PF/PF-Assgn40.py
@@ -0,0 +1,15 @@
+'''
+Write a recursive function, is_palindrome() to find out whether a string is a palindrome or not. 
+The function should return true, if it is a palindrome. Else it should return false.
+'''
+#PF-Assgn-40
+def is_palindrome(word):
+    pass
+    #Remove pass and write your logic here
+
+#Provide different values for word and test your program
+result=is_palindrome("MadAMa")
+if(result):
+    print("The given word is a Palindrome")
+else:
+    print("The given word is not a Palindrome")
