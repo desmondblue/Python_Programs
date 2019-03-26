676f3b8fcf86a6b0b663602af681f9187525c395
Ankush Kalsi <ankushk456@gmail.com>
2019-03-26 20:07:45 +0530

    Create PF-Assgn55.py

---
+++ infytq/PF/PF-Assgn55.py
@@ -0,0 +1,5 @@
+'''
+Write a python program to help an airport manager to generate few statistics based on the ticket details available for a day.
+Go through the below program and complete it based on the comments mentioned in it.
+Note: Perform case sensitive string comparisons wherever necessary.
+'''
